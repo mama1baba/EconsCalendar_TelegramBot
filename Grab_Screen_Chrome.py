e33635ebaadd00b0f38d6324a657b510dc936dc1
Wen Jun Ong <marcus.jun94@gmail.com>
2019-08-04 11:32:19 +0800

    Update Grab_Screen_Chrome.py

--- Grab_Screen_Chrome.py
+++ Grab_Screen_Chrome.py
@@ -1,51 +1,42 @@
-#!/usr/bin/env python
-# coding: utf-8
+'''
+-----------------------------
+This script is created to screenshot Investing.com webpage via selenium chrome driver,
+save into local machine and send it out via telegram bot to respective channels
+-----------------------------
+'''
 
-# In[1]:
-
-
-from selenium import webdriver
+from selenium import webdriver #webdriver to launch chrome
 from datetime import date
-import telegram
+import telegram #for telegram bot
 
 
-# In[2]:
-
-
-# Save screenshot from URL in current directory
 URL = 'https://ph.investing.com/economic-calendar/'
 
-
+# Launch Chrome without Header
 options = webdriver.ChromeOptions()
 options.headless = True
+
+# Save screenshot from URL in current directory
 driver = webdriver.Chrome(options = options)
 driver.get(URL)
 
-S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X) # to obtain dynamic width and height
 driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
 driver.find_element_by_tag_name('body').screenshot("EconomicCalendar" + str(date.today()) + ".png")
-driver.quit()
+driver.quit() # remember to quit driver
 
-
-# In[3]:
-
-
+#Read your telegram bot API keys
 f = open('Token.txt', mode = 'r')
 token = f.read()
 
 bot = telegram.Bot(token = token)
 
-
-# In[5]:
-
-
+# Send out screenshot with link as captions ; chat_id = channel_name
 bot.sendDocument(chat_id = "@testnews32", document = open("EconomicCalendar" + str(date.today()) + ".png", 
                                                          mode = 'rb'),
                 caption = 'Source:' + URL)
     
 
-
-# In[ ]:
 
 
 
